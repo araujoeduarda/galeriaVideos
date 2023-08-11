0fad93ed236c8a305b3aceddff0a3a952e50950d
Maria Eduarda Araujo <maeduardaaraujo01@gmail.com>
2023-08-11 18:26:18 -0300

    commit 8

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ document.addEventListener("DOMContentLoaded", function () {
     }, index * 300); 
   });
 });
+    // Função p rolagem suave
+    function scrollToElement(elementId) {
+      const element = document.getElementById(elementId);
+      if (element) {
+        element.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    }
+
+    // ouvintes de clique aos links da sidebar
+    const sidebarLinks = document.querySelectorAll('.sidebar-menu a');
+    sidebarLinks.forEach(link => {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+        const target = link.getAttribute('href').substring(1);
+        scrollToElement(target);
+      });
+    });
