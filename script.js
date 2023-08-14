736d03d635aa145eee490c7f28a43d0ba76368e7
Maria Eduarda Araujo <maeduardaaraujo01@gmail.com>
2023-08-14 18:16:27 -0300

    commit linsk

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const videoContainers = document.querySelectorAll(".video-container");
-
-  videoContainers.forEach(function (container, index) {
-    setTimeout(function () {
-      container.classList.add("aparecer"); 
-    }, index * 300); 
-  });
-});
-    // Função p rolagem suave
-    function scrollToElement(elementId) {
-      const element = document.getElementById(elementId);
-      if (element) {
-        element.scrollIntoView({
-          behavior: 'smooth',
-          block: 'start'
-        });
-      }
-    }
 
     // ouvintes de clique aos links da sidebar
     const sidebarLinks = document.querySelectorAll('.sidebar-menu a');
@@ -28,4 +9,19 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
 
+    function scrollToSection(sectionId) {
+      var section = document.getElementById(sectionId);
+      if (section) {
+        section.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    }
+
+    
+
+    function loadVideo(videoId) {
+      const videoContainer = document.getElementById("video-container");
+      videoContainer.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>`;
+    }
     
