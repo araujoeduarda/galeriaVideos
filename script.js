4a4c57ac1324c3b83a75fa8e5fb379061dbaa8d4
Maria Eduarda Araujo <maeduardaaraujo01@gmail.com>
2023-08-14 18:39:51 -0300

    commit divs

--- script.js
+++ script.js
@@ -21,8 +21,10 @@
     
 //Aparecimento do video//
 
-    function loadVideo(videoId) {
-      const videoContainer = document.getElementById("video-container");
-      videoContainer.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>`;
-    }
-    
+function loadVideo(videoId, containerId) {
+  const videoContainer = document.getElementById(containerId);
+  videoContainer.innerHTML = `
+    <div class="video-wrapper">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
+    </div>`;
+}
